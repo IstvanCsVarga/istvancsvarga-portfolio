d28e4615ccb9ec9d5cf74088ed10a90c453b8288
istvan.csaba.varga <istvan.csaba.varga@accenture.com>
2023-12-16 15:55:54 +0100

    Refactor Ball component to remove unused code and simplify rendering logic

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useState, useEffect } from "react";
+import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import {
   Decal,
@@ -10,106 +10,38 @@ import {
 
 import CanvasLoader from "../Loader";
 
-const SimpleBall = (props) => {
-  return (
-    <mesh castShadow receiveShadow scale={1.75}>
-      <icosahedronGeometry args={[1, 0]} /> 
-      <meshStandardMaterial color="#fff8eb" />
-    </mesh>
-  );
-};
-
-
-// const Ball = (props, isMobile) => {
-//   const [decal] = useTexture([props.imgUrl]);
-
-//   return (
-//     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-//       <ambientLight intensity={0.25} />
-//       <directionalLight position={[0, 0, 0.05]} />
-//       <mesh castShadow receiveShadow scale={2.75}>
-//         <icosahedronGeometry args={[1, 1]} />
-//         <meshStandardMaterial
-//           color="#fff8eb"
-//           polygonOffset
-//           polygonOffsetFactor={-5}
-//           flatShading
-//         />
-//         <Decal
-//           map={decal}
-//           position={[0, 0, 1]}
-//           flatShading
-//           rotation={[2 * Math.PI, 0, 6.25]}
-//         />
-//       </mesh>
-//     </Float>
-//   );
-// };
-
-const Ball = ({ imgUrl, isMobile }) => {
-  const [decal] = useTexture([imgUrl]);
-
-  const geometryArgs = isMobile ? [1, 0] : [1, 1]; // Less detail for mobile
-  const floatIntensity = isMobile ? 1 : 2; // Less intense floating effect for mobile
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={floatIntensity}>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={geometryArgs} />
+        <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
           color="#fff8eb"
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
         />
-        {!isMobile && ( // Only render Decal if not on mobile
-          <Decal
-            map={decal}
-            position={[0, 0, 1]}
-            flatShading
-            rotation={[2 * Math.PI, 0, 6.25]}
-          />
-        )}
+        <Decal
+          map={decal}
+          position={[0, 0, 1]}
+          flatShading
+          rotation={[2 * Math.PI, 0, 6.25]}
+        />
       </mesh>
     </Float>
   );
 };
 
-
 const BallCanvas = ({ icon }) => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    // Set an event listener to check if the user is on mobile or not
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    // Set the initial state of the isMobile variable
-    setIsMobile(mediaQuery.matches);
-
-    // Define a callback function to handle
-    // the change of the media query
-    const handleMediaQueryChange = (event) => {
-      setIsMobile(event.matches);
-
-      // Add the callback function as a listener to the query
-      mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-      // Remove the callback function from the query
-      return () => {
-        mediaQuery.removeEventListener("change", handleMediaQueryChange);
-      };
-    };
-  }, []);
-
   return (
     <Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
-        {/* <Ball imgUrl={icon} /> */}
-        {/* {isMobile ? <SimpleBall /> : <Ball imgUrl={icon} />} */}
-        <Ball imgUrl={icon} isMobile={isMobile} />
+        <Ball imgUrl={icon} />
       </Suspense>
 
       <Preload all />
